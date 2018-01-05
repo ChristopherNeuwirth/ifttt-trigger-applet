578d278b0bf1c60159b3d65921533b90dcd4353d
Christopher Neuwirth <christopher.neuwirth@mhp.com>
2018-01-05 14:35:03 +0100

    🔧 Passed process.env variables to child process

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 let fork = require('child_process').fork;
+let path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, 'environment/.env')});
 
 module.exports = (req, res) => {
+
   fork('./src/main.js', [], {
     env: {
-      ENV: 'PROD'
+      ENV: 'PROD',
+      BASE: process.env.BASE,
+      URL: process.env.URL,
+      APPIDDEV: process.env.APPIDDEV,
+      APPIDPROD: process.env.APPIDPROD,
+      ENDPOINT: process.env.ENDPOINT,
+      TOKEN: process.env.TOKEN,
+      COOKIE: process.env.COOKIE,
+      REFERER: process.env.REFERER
     }
   });
-  res.end('You have started the cron job 🙌');
+  return 'You have started the cron job.';
 }
